e148f5472fed1d1ab58b6d283d11e2fafd6414af
Slava <76904504+bnq7@users.noreply.github.com>
2025-02-09 19:34:20 +0300

    Update RatingsController.java

--- webresources/123/src/main/java/com/webresources/controller/RatingsController.java
+++ webresources/123/src/main/java/com/webresources/controller/RatingsController.java
@@ -1,26 +1,53 @@
 package com.webresources.controller;
 
 import com.webresources.controller.main.Attributes;
-import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.ui.Model;
 
-@Controller
-@RequestMapping("/ratings")
-public class RatingsController extends Attributes {
+import java.util.List;
+
+@RestController
+@RequestMapping("/analysis/ranking")
+public class RankingController extends Attributes {
+
+    @Autowired
+    private ScoreRepository scoreRepo;
+
 
     @GetMapping
-    public String ratings(Model model) {
+    public List<SiteRating> getRanking() {
+      
+        return scoreRepo.findAll();
+    }
+
+    public static class SiteRating {
+        private String siteName;
+        private float overallRating;
+
     
-        AddAttributes(model);
 
-   
-        model.addAttribute("controllerInfo", "Этот маршрут принадлежит контроллеру RatingsController с роутом /ratings");
+        public SiteRating(String siteName, float overallRating) {
+            this.siteName = siteName;
+            this.overallRating = overallRating;
+        }
 
-        model.addAttribute("ratings", scoreRepo.findAll());
+        public String getSiteName() {
+            return siteName;
+        }
 
-    
-        return "ratings";
+        public void setSiteName(String siteName) {
+            this.siteName = siteName;
+        }
+
+        public float getOverallRating() {
+            return overallRating;
+        }
+
+        public void setOverallRating(float overallRating) {
+            this.overallRating = overallRating;
+        }
     }
 }
